08a338c5db5d4f6277bc540e721dd04040f12ada
Fazeel Khalid <fazeelkhalid9@gmail.com>
2019-08-04 07:48:29 +0500

    Create reverse string.cpp

---
+++ reverse string/reverse string.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	char a[10];
+	printf("enter you name ");
+	scanf("%s", &a);
+		
+	for(int i=10; i>=0;i-- ) { 
+		printf("%C",a[i]);
+	}
+	
+	return 0;
+}
+panding
