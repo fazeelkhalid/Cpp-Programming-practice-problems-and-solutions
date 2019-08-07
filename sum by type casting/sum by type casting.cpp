d5b82d3c8386100f5f037ef30b44aed4822f20f8
Fazeel Khalid <fazeelkhalid9@gmail.com>
2019-08-07 13:22:56 +0500

    Create sum by type casting.cpp

---
+++ sum by type casting/sum by type casting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	float first_Number = 10.398;
+	float second_Number = 98.548;
+	float sum;
+	
+	sum = first_Number + second_Number;
+	
+	cout<<"Sum: "<< (int)sum; 
+	
+	
+}
