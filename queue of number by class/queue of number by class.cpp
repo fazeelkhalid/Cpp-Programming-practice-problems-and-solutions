c1d320570deb481b3e69d93ccb58828c734cc248
Fazeel Khalid <fazeelkhalid9@gmail.com>
2019-08-08 17:38:11 +0500

    Add some function
    some functions are:
    deQueue
    isFull
    isEmpty
    top
    bottom

--- queue of number by class/queue of number by class.cpp
+++ queue of number by class/queue of number by class.cpp
@@ -11,9 +11,43 @@ class Queue {
 	}
 	
 	void enqueue(int a) {
-		array[size] = a;
-		size++;
-	}	
+		if(size < 5) {
+			array[size] = a;
+			size++;
+		}
+		else
+			cout<<"Queue full";
+	}
+	
+	int dequeue() {
+		size--;
+		return array[size];
+	}
+	
+	bool isFull() {
+		if(this->size < 5) {
+			cout<<"Queue not full";
+		}
+		else
+			cout<<"Queue is full";
+	}
+		
+	void isEmpty() {
+		if(size == 0) {
+			cout<<"Queue is empty";
+		}
+		else
+			cout<<"Queue not empty";
+	}
+
+	int top() {
+		return array[--size];						
+	}
+	
+	int bottom() {
+		return array[0];
+	}
+	
 };
 
 int main() {
@@ -22,7 +56,17 @@ int main() {
 	q.enqueue(109);
 	q.enqueue(19);
 	q.enqueue(9);
+ 	q.enqueue(92);
+ 	q.enqueue(39);
+ 	q.enqueue(392);
  	
- 	return 0;
  	
- }
+ 	q.isEmpty();
+ 	
+	cout<<endl;
+	q.dequeue();
+	cout<<q.top();
+ 	
+	return 0;
+
+}
